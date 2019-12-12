a583fbc16f97327d5b12c924cd20cc915f1abeb7
ssooya90 <ssooya90@naver.com>
2019-12-13 00:45:35 +0900

    jap 등록 테스트

---
+++ src/main/java/com/ssooya/book/springboot/service/PostsService.java
@@ -0,0 +1,20 @@
+package com.ssooya.book.springboot.service;
+
+import com.ssooya.book.springboot.domain.posts.PostsRepository;
+import com.ssooya.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+	private final PostsRepository postsRepository;
+
+	@Transactional
+	public Long save(PostsSaveRequestDto requestDto) {
+		return postsRepository.save(requestDto.toEntity()).getId();
+	}
+
+}
