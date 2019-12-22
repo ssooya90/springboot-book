279f6222b10aff3e4936865b69cb5887b987d7cb
ssooya90 <ssooya90@naver.com>
2019-12-23 01:34:33 +0900

    4장 끝

--- src/test/java/com/ssooya/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/ssooya/book/springboot/web/HelloControllerTest.java
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(HelloController.class)
 public class HelloControllerTest {
 
 	@Autowired
